3246984878e754e720a26e696b1ff160e2c19493
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-15 10:25:21 +0100

    complete details package view

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -3,16 +3,30 @@
 @section('content')
 
 <div class="container">
-<h1 class="title">Pacchetti Viaggi</h1>
+<h1 class="title text-center py-5">Pacchetti Viaggi</h1>
 <div class="pacchetti_viaggi">
 @foreach ($packages as $package)
 
+<div class="card text-center my-5">
+  <div class="card-body">
+    <h3 class="card-title">{{$package->hotel}}</h3>
+    <div class="card-text d-flex py-3">
+        <div class="col-4">
+            <p><strong>Posizione:</strong> {{$package->city}}</p>
+        </div>
+        <div class="col-4">
+            <h5><strong>Prezzo:</strong> {{$package->price}}</h5>
+        </div>
+        <div class="col-4">
+            <p><strong>Voto:</strong> <span class="vote">{{$package->vote}}</span></p>
+        </div>
+    </div>
+    <a href="#" class="btn offer">Vedi offerta</a>
+  </div>
+</div>
+
             <div class="info_voyage">
-                <h2>Info Voli</h2>
-                <p>{{$package->city}}</p>
-                <p>{{$package->hotel}}</p>
-                <p>{{$package->price}}</p>
-                 <p>{{$package->vote}}</p>
+
             </div>
 
 
