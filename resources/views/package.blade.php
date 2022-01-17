a3de834b1a5796a43201dba99434d9f108335bef
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-17 15:13:35 +0100

    update table travel_packages

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -8,20 +8,20 @@
 @foreach ($packages as $package)
 
 <div class="card text-center my-5">
-  <div class="card-body">
-    <h3 class="card-title">{{$package->hotel}}</h3>
-    <div class="card-text d-flex py-3">
-        <div class="col-4">
-            <p><strong>Posizione:</strong> {{$package->city}}</p>
-        </div>
-        <div class="col-4">
-            <h5><strong>Prezzo:</strong> {{$package->price}}</h5>
-        </div>
-        <div class="col-4">
-            <p><strong>Voto:</strong> <span class="vote">{{$package->vote}}</span></p>
-        </div>
-    </div>
-    <a href="#" class="btn offer">Vedi offerta</a>
+  <div class="card-body d-flex">
+      <div class="col-6">
+          <img class="w-100 h-100" src="{{$package->image}}" alt="">
+      </div>
+      <div class="col-6 p-5">
+            <h3 class="card-title">{{$package->hotel}}</h3>
+                <div class="info d-flex py-5 justify-content-between">
+                    <p><strong>Posizione:</strong> {{$package->city}}</p>
+                    <h5><strong>Prezzo:</strong> {{$package->price}}</h5>
+                    <p><strong>Voto:</strong> <span class="vote">{{$package->vote}}</span></p>
+                </div>
+                <a href="#" class="btn offer">Vedi offerta</a>
+      </div>
+
   </div>
 </div>
 
